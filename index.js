a55dd9fb0a742a2482e8c96d8627817fc32e989d
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-19 14:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-var kittens = ["Milo","Otis","Garfield"] 
+var kittens = ["Milo","Otis","Garfield"]
 
 function destructivelyAppendKitten(name) {
   return kittens.push("Ralph");
