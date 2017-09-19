c7ff68740f7607a8f095a8c7661dcdc7db77ac6c
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-19 14:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,15 +14,18 @@ function destructivelyRemoveFirstKitten() {
   return kittens.shift()
 }
 function appendKitten(name) {
-//  var newKittens = []
-
+  newKittens = [...kittens, name]
+  return newKittens
 }
 function prependKitten(name) {
-
+  newKittens = [name, ...kittens]
+  return newKittens
 }
 function removeLastKitten() {
-
+  newKittens = kittens.slice(1)
+  return newKittens
 }
 function removeFirstKitten() {
-
+  newKittens = kittens.slice(0, kittens.length - 1)
+  return newKittens
 }
