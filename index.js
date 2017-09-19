b9c0925b96821bdc462e3d40263f245b0e54a240
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-19 14:00:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const app = "I don't do much."
 const kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten (name) {
-  
+
 }
 function destructrivelyPrependKitten (name) {
-  
+
 }
 function destructivelyRemoveLastKitten() {
-  
+
 }
 function destructivelyRemoveFirstKitten() {
-  
+
 }
 function appendKitten(name) {
-  
+
 }
 function prependKitten(name) {
-  
+
 }
 function removeLastKitten() {
-  
+
 }
 function removeFirstKitten() {
-  
+
 }
